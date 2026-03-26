ed6745c82ad6e748a2828452a032eaea99ab346c
omsolanki271 <omsolanki271@gmail.com>
2026-03-27 00:08:03 +0530

    Add getter and setter with constructor in Bean Class

--- 01-spring-boot-beans-1/src/main/java/com/springboot/beans/Student.java
+++ 01-spring-boot-beans-1/src/main/java/com/springboot/beans/Student.java
@@ -1,5 +1,45 @@
 package com.springboot.beans;
 
 public class Student {
+	private String name;
+	private int rollno;
+	private int marks;
 
+	public Student(String name, int rollno,int marks)
+	{
+		this.name = name;
+		this.rollno = rollno;
+		this.marks = marks;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	
+	public void displaymarks()
+	{
+		System.out.println("Name :" + name );
+		System.out.println("Roll No :" + rollno );
+		System.out.println("Marks :" + marks );
+	}
 }
