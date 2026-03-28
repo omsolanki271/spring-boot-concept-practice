1a08ce8be2fa605553ea1033f5201ee54f6adf97
omsolanki271 <omsolanki271@gmail.com>
2026-03-29 01:50:55 +0530

    Successfully Table create and Row insert Proper in Table

--- 03-hibernate-crud/src/main/java/com/hibernet/main/App.java
+++ 03-hibernate-crud/src/main/java/com/hibernet/main/App.java
@@ -1,6 +1,5 @@
 package com.hibernet.main;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,19 +8,33 @@ import org.hibernate.cfg.Configuration;
 
 import com.hibernet.entity.User;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	
-    	
-        Configuration configuration = new Configuration();
-        configuration.configure("hibernate.cfg.xml");
-        
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        
-        
-    }
+public class App {
+	public static void main(String[] args) {
+		User user = new User();
+		user.setId(1);
+		user.setName("om");
+		user.setEmail("om@gmail.com");
+		user.setPassword("123");
+		user.setGender("M");
+		user.setCity("bvd");
+
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+
+		try 
+		{
+			session.save(user);
+			transaction.commit();
+		} 
+		catch (Exception e) 
+		{
+			transaction.rollback();
+			e.printStackTrace();
+		}
+
+	}
 }
