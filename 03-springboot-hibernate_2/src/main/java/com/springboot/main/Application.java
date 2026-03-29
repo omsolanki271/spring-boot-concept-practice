1db265ac40601fa2982898a91b336c7117b9e1c8
omsolanki271 <omsolanki271@gmail.com>
2026-03-29 19:44:28 +0530

    Perform Select Operation using ApplicationContext

--- 03-springboot-hibernate_2/src/main/java/com/springboot/main/Application.java
+++ 03-springboot-hibernate_2/src/main/java/com/springboot/main/Application.java
@@ -18,7 +18,28 @@ import com.springboot.repository.Dboperation;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext context =  SpringApplication.run(Application.class, args);
+		Dboperation bean = context.getBean(Dboperation.class);
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter ID to fetch data: ");
+		Long selectId = sc.nextLong();
+
+		User user2 = bean.getUserdetail(selectId);
+		if (user2 != null) {
+				System.out.println(user2.getId());
+				System.out.println(user2.getName());
+				System.out.println(user2.getCity());
+				System.out.println(user2.getGender());
+				System.out.println(user2.getEmail());
+				System.out.println(user2.getPassword());
+		}
+		else 
+		{
+			System.out.println("No Founded...! " + selectId);
+		}
+		
 
 	}
 }
