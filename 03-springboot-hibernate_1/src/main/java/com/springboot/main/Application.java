aa09ee961c21168bc71f63d0db1c8a8c741ae38a
omsolanki271 <omsolanki271@gmail.com>
2026-03-29 18:12:24 +0530

    Configuration file Proper working and fetch in Main Class No Error

--- 03-springboot-hibernate_1/src/main/java/com/springboot/main/Application.java
+++ 03-springboot-hibernate_1/src/main/java/com/springboot/main/Application.java
@@ -1,5 +1,11 @@
 package com.springboot.main;
 
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +14,17 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("hello");
+		
+		
+		Configuration configuration = new Configuration();
+		configuration.configure("/com/springboot/config/hibernate.cfg.xml");
+		
+		
+		SessionFactory factory = configuration.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		System.out.println("working...");
 	}
 
 }
