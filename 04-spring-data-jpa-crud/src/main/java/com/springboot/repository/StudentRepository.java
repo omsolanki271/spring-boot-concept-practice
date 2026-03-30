0e0f7a7bea1cdde3d49423a4ee3727935ccc3307
omsolanki271 <omsolanki271@gmail.com>
2026-03-30 22:33:23 +0530

    Add StudentRepository interface to extend interface JpaRepository in repository package.

---
+++ 04-spring-data-jpa-crud/src/main/java/com/springboot/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.entities.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
