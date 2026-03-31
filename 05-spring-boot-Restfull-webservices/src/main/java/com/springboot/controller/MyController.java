23f9f98a4f7620292732481f1fa34a061810c6fc
omsolanki271 <omsolanki271@gmail.com>
2026-04-01 02:14:52 +0530

    Configuration add properties for database

--- 05-spring-boot-Restfull-webservices/src/main/java/com/springboot/controller/MyController.java
+++ 05-spring-boot-Restfull-webservices/src/main/java/com/springboot/controller/MyController.java
@@ -2,13 +2,16 @@ package com.springboot.controller;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.entities.User;
 
 @RestController
 public class MyController {
 
 	@PostMapping("/user")
-	public String addUserDetails()
+	public String addUserDetails(@RequestBody User user)
 	{
 		return "index";
 	}
